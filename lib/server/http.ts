6d74feade9e049d59ea7dea4f5b5dc3c43697422
onemarc <onedev0101@gmail.com>
2026-08-03 21:31:00 +0300

    feat: add function to check for Postgres exclusion violations

--- lib/server/http.ts
+++ lib/server/http.ts
@@ -62,3 +62,12 @@ export function isPostgresUniqueViolation(
     error.constraint === constraint
   );
 }
+
+export function isPostgresExclusionViolation(error: unknown) {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "code" in error &&
+    error.code === "23P01"
+  );
+}
